2786387570513f477fb1b2d3471ca81843641606
KacperL1ga <162919256+KacperL1ga@users.noreply.github.com>
2025-04-27 19:59:42 +0200

    Utworzenie klasy Sensor

---
+++ Sensor.py
@@ -0,0 +1,70 @@
+
+import random
+import time
+
+class Sensor:
+    def __init__(self, sensor_id, name, unit, min_value, max_value, frequency=1):
+        """
+        Inicjalizacja czujnika.
+
+        :param sensor_id: Unikalny identyfikator czujnika
+        :param name: Nazwa lub opis czujnika
+        :param unit: Jednostka miary (np. '°C', '%', 'hPa', 'lux')
+        :param min_value: Minimalna wartość odczytu
+        :param max_value: Maksymalna wartość odczytu
+        :param frequency: Częstotliwość odczytów (sekundy)
+        """
+        self.sensor_id = sensor_id
+        self.name = name
+        self.unit = unit
+        self.min_value = min_value
+        self.max_value = max_value
+        self.frequency = frequency
+        self.active = True
+        self.last_value = None
+
+    def read_value(self):
+        """
+        Symuluje pobranie odczytu z czujnika.
+        W klasie bazowej zwraca losową wartość z przedziału [min_value, max_value].
+        """
+        if not self.active:
+            raise Exception(f"Czujnik {self.name} jest wyłączony.")
+
+        value = random.uniform(self.min_value, self.max_value)
+        self.last_value = value
+        return value
+
+    def calibrate(self, calibration_factor):
+        """
+        Kalibruje ostatni odczyt przez przemnożenie go przez calibration_factor.
+        Jeśli nie wykonano jeszcze odczytu, wykonuje go najpierw.
+        """
+        if self.last_value is None:
+            self.read_value()
+
+        self.last_value *= calibration_factor
+        return self.last_value
+
+    def get_last_value(self):
+        """
+        Zwraca ostatnią wygenerowaną wartość, jeśli była wygenerowana.
+        """
+        if self.last_value is None:
+            return self.read_value()
+        return self.last_value
+
+    def start(self):
+        """
+        Włącza czujnik.
+        """
+        self.active = True
+
+    def stop(self):
+        """
+        Wyłącza czujnik.
+        """
+        self.active = False
+
+    def __str__(self):
+        return f"Sensor(id={self.sensor_id}, name={self.name}, unit={self.unit})"
